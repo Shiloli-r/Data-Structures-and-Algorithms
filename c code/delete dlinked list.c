dd4565b1149f116565f6938092d37728d59bb7a7
Shiloli-r <ronaldshiloli@gmail.com>
2019-10-29 00:36:00 +0300

    data structures assingments in c

--- c code/delete dlinked list.c
+++ c code/delete dlinked list.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -66,72 +65,3 @@ void displaydoublelist(node * head){
         p= p->next;
     }
 }
-=======
-#include <stdio.h>
-#include <stdlib.h>
-
-
-typedef struct node {
-    int data;
-    struct node *next;
-    struct node *prev;
-}node;
-void displaydoublelist(node * head);
-
-int main(){
-    node *HEAD = NULL;
-     int n = 8;
-    int numbers[8] = {8, 5, 7, 3, 4, 1, 2, 9};
-
-
-    node * q = NULL;    // will be the new node to be created
-    node * p = NULL;    // will be the trailing pointer
-    for(int i=0;i<n;i++){
-       q = (node*)malloc(sizeof(node));
-        q->next = NULL;
-
-       q->data = numbers[i];
-
-        if(HEAD == NULL){
-            HEAD  = q;
-            q->prev = NULL;
-        }else{
-            p = HEAD;
-            while( p->next != NULL){
-                p = p->next;
-            }
-            p->next = q; //link p to the right link of q
-            q->prev = p; //link q to the left link of p
-        }
-    }
-
-    printf("Double Linked List: ");
-    displaydoublelist(HEAD);
-    printf("\n\n");
-
-    /* DELETION */
-    int x;
-    printf("Enter the element to delete: ");
-    scanf("%d", &x);
-    p = HEAD;
-    while(p != NULL){
-            if(p->data == x){
-                (p->prev)->next = p->next; //reassigning the right link
-                (p->next)->prev = p->prev; //reassigning the left link
-            }
-            p = p->next;  //keep moving to the next p
-            }
-    printf("\n New Double Linked list: ");
-    displaydoublelist(HEAD);
-    printf("\n");
-
-}
-
-void displaydoublelist(node * head){
-    node * p = head;
-    while(p != NULL){
-        printf("%d <-> ",p->data);
-        p= p->next;
-    }
-}
->>>>>>> bdc6a99ff6e0ded6c1c233a980196e347b1f9eb6
--- c code/double linked list.c
+++ c code/double linked list.c
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -57,63 +57,3 @@ void displaydoublelist(node * head){
 }
 
 
-=======
-#include <stdio.h>
-#include <stdlib.h>
-
-typedef struct node {
-    int data;
-    struct node *next;
-    struct node *prev;
-}node;
-
-node * createDoublelist(int n); //function to create list
-void displaydoublelist(node * head);
-int main(){
-     node *HEAD = NULL;  //list initially empty
-    int n;
-    printf("Enter the size of the list: ");
-    scanf("%d", &n);
-    HEAD = createDoublelist(n);
-    displaydoublelist(HEAD);
-
-return 0;
-}
-node * createDoublelist(int n){
-    node * q = NULL;    // will be the new node to be created
-    node * p = NULL;    // will be the trailing pointer
-    node * head = NULL; //head of the list, which will be returned
-
-    for(int i=0; i<n; i++){
-        q = (node*)malloc(sizeof(node));
-        q->next = NULL;
-
-        printf("Enter element %d: ", i+1);
-        scanf("%d",&(q->data));
-
-        if(head == NULL){
-            head  = q;
-            q->prev = NULL;
-        }else{
-            p = head;
-            while( p->next != NULL){
-                p = p->next;
-            }
-            p->next = q; //link p to the right link of q
-            q->prev = p; //link q to the left link of p
-        }
-    }
-
-return head;
-}
-
-void displaydoublelist(node * head){
-    node * p = head;
-    while(p != NULL){
-        printf("%d <-> ",p->data);
-        p= p->next;
-    }
-}
-
-
->>>>>>> bdc6a99ff6e0ded6c1c233a980196e347b1f9eb6
